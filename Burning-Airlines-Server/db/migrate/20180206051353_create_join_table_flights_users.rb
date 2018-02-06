05bfda5a291b0a6eed01254206cd795592e7188d
tnpriyanka12 <tnpriyanka12@gmail.com>
2018-02-07 00:08:58 +1100

    Typos and associations fixed(i think)

--- Burning-Airlines-Server/db/migrate/20180206051353_create_join_table_flights_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableFlightsUsers < ActiveRecord::Migration[5.1]
-  def change
-    create_join_table :flights, :users do |t|
-      # t.index [:flight_id, :user_id]
-      # t.index [:user_id, :flight_id]
-    end
-  end
-end
